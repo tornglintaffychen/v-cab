3a00220515ec9a626a25753945e083ccaa28c66b
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-06-08 19:36:42 -0400

    added cart bottle logo images to choose from pull vcag logo

--- browser/js/common/directives/cart/cart.js
+++ browser/js/common/directives/cart/cart.js
@@ -3,9 +3,7 @@
 app.directive('cart', function() {
   return{
 		restrict: 'E',
-		templateUrl:"/cart.html",
-		link: function(scope, element, attrs) {
-			// scope.numOfItems = ProductFactory.numOfItems; //TODO pending Katie's code
-		}
+		templateUrl:'/js/common/directives/cart/cart.html'
+		
 	}
 })
