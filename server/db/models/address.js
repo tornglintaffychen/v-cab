6041e296f741081373cce67cc538158bb793621f
Svinci131 <svinci131@gmail.com>
2016-06-08 20:38:39 -0400

    before megre master into view so I have product factory

--- server/db/models/address.js
+++ server/db/models/address.js
@@ -1,30 +1,52 @@
-'use strict';
-var crypto = require('crypto');
-var _ = require('lodash');
-var Sequelize = require('sequelize');
-
-module.exports = function (db) {
-
-    db.define('address', {
-        street: {
-            type: Sequelize.STRING
-        },
-
-        apartment: {
-            type: Sequelize.STRING,
-            defaultValue: ''
-        },
-        town: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        // state: {
-        //     type: Sequelize.ENUM('Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming', 'American Samoa', 'Guam', 'Northern Marianas Islands', 'Puerto Rico', V'irgin Islands', 'Armed Forces Americas', 'Armed Forces Africa', 'Armed Forces Europe', 'Armed Forces Middle East', 'Armed Forces Pacific'),
-        //     allowNull: false
-        // },
-        zip:{
-            type: Sequelize.STRING,
-            allowNull: false
-        }
-    });
-};
+// // 'use strict';
+// // var crypto = require('crypto');
+// // var _ = require('lodash');
+// // var Sequelize = require('sequelize');
+//
+// // module.exports = function (db) {
+//
+// //     db.define('address', {
+// //         street: {
+// //             type: Sequelize.STRING
+// //         },
+//
+// <<<<<<< HEAD
+//         apartment: {
+//             type: Sequelize.STRING,
+//             defaultValue: ''
+//         },
+//         town: {
+//             type: Sequelize.STRING,
+//             allowNull: false
+//         },
+//         // state: {
+//         //     type: Sequelize.ENUM('Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming', 'American Samoa', 'Guam', 'Northern Marianas Islands', 'Puerto Rico', V'irgin Islands', 'Armed Forces Americas', 'Armed Forces Africa', 'Armed Forces Europe', 'Armed Forces Middle East', 'Armed Forces Pacific'),
+//         //     allowNull: false
+//         // },
+//         zip:{
+//             type: Sequelize.STRING,
+//             allowNull: false
+//         }
+//     });
+// };
+// =======
+// //         apartment: {
+// //             type: Sequelize.STRING,
+// //             defaultValue: ''
+// //         },
+// //         town: {
+// //             type: Sequelize.STRING,
+// //             allowNull: false
+//
+// //         },
+// //         state: {
+// //             type: Sequelize.ENUM('Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming', 'American Samoa', 'Guam', 'Northern Marianas Islands', 'Puerto Rico', V'irgin Islands', 'Armed Forces Americas', 'Armed Forces Africa', 'Armed Forces Europe', 'Armed Forces Middle East', 'Armed Forces Pacific'),
+// //             allowNull: false
+// //         },
+// //         zip:{
+// //             type: Sequelize.STRING,
+// //             allowNull: false
+// //         }
+// //     }
+// // };
+// >>>>>>> f91fc5daceed2c6536ba668a0ba2d3c86def1f29
--- server/db/models/category.js
+++ server/db/models/category.js
@@ -1,10 +1,8 @@
 'use strict';
-var crypto = require('crypto');
-var _ = require('lodash');
 var Sequelize = require('sequelize');
 
 module.exports = function (db) {
-	db.define('category', {
+    db.define('category', {
         title: {
             type: Sequelize.STRING,
             allowNull: false
