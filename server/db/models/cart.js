803d11c2e9c8a015d5fe590587927065ecec05f7
Torng-Lin Chen <tornglinchen@Torng-Lins-MacBook-Pro.local>
2016-06-09 19:21:31 -0400

    remove force true, remove productId, orderId

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -3,13 +3,6 @@ var Sequelize = require('sequelize');
 
 module.exports = function (db) {
     db.define('OrderProduct', {
-        orderId: {
-            type: Sequelize.INTEGER
-
-        },
-        productId: {
-            type: Sequelize.INTEGER
-        },
         quantity: {
             type: Sequelize.INTEGER
         },
--- server/main.js
+++ server/main.js
@@ -21,9 +21,7 @@ var startServer = function () {
 
 };
 
-db.sync({
-    force: true
-}).then(createApplication).then(startServer).catch(function (err) {
+db.sync().then(createApplication).then(startServer).catch(function (err) {
     console.error(chalk.red(err.stack));
     process.kill(1);
 });
