0d1bb202f74095d546c4f8ad340c6f9f67179ece
Torng-Lin Chen <tornglinchen@Torng-Lins-MacBook-Pro.local>
2016-06-08 20:39:43 -0400

    added order post route

--- server/app/routes/Orders/index.js
+++ server/app/routes/Orders/index.js
@@ -5,27 +5,35 @@ var rootPath = '../../../';
 var Order = require(rootPath + 'db').Order;
 
 // Only admins
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     Order.findAll({
             where: req.query
         })
-        .then(function(orders) {
+        .then(function (orders) {
             res.json(orders)
         })
         .catch(next);
 })
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
     Order.findById(req.params.id)
-        .then(function(order) {
+        .then(function (order) {
             res.json(order)
         })
         .catch(next);
 })
 
+router.post('/', function (req, res, next) {
+    Order.create(req.body)
+        .then(function (order) {
+            res.json(order)
+        })
+        .catch(next)
+})
+
 // admin should be able to edit everything in the order
 // users should be able to cancel order 30 mins limit
-router.put('/:id', function(req, res, next) {
+router.put('/:id', function (req, res, next) {
     Order.update(req.body, {
             where: {
                 id: req.params.id
