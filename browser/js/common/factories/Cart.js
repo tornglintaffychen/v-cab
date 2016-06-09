2570621560a296a77e65193e76b58757d352ae3b
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-06-08 20:37:01 -0400

    Started working on Factory, need to pull new master

--- browser/js/common/factories/Cart.js
+++ browser/js/common/factories/Cart.js
@@ -1,12 +1,15 @@
 //Factory
 
 //communicates between main page and the cart icon overlay
-app.factory('CartFactory', function() {
+app.factory('CartFactory', function($http) {
 	function getData(response) {
 		return response.data;
 	}
 	return {
 		//have a variable to show cart count
+		itemCount: function (OrderId) {
+			$http.get('/api/orders')
+		},
 
 	}
 })
