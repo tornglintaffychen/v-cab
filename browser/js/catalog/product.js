b7e4041dcd412fab0a5710bfb5640947f6028d2c
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-06-09 14:48:07 -0400

    Trying to get the view to show when navigate to '/products', problem with route

---
+++ browser/js/catalog/product.js
@@ -0,0 +1,39 @@
+'use strict'
+//state for single product
+app.config(function ($stateProvider) {
+    $stateProvider.state('product', {
+        url: '/products/:id',
+        templateUrl: 'js/catalog/products.html',
+        controller: 'ProductCtrl',
+        resolve: {
+					//LA: changed from oneProduct to singleProduct
+            singleProduct: function (ProductFactory, $stateParams) {
+                return ProductFactory.fetchById($stateParams.id);
+            }
+        }
+    })
+})
+
+// state for all products
+app.config(function ($stateProvider) {
+    $stateProvider.state('home.products', {
+        url: '/products',
+        templateUrl: 'js/catalog/products.html',
+        resolve: {
+            allProducts: function (ProductFactory) {
+                return ProductFactory.fetchAll();
+            }
+        }
+    });
+});
+
+
+// //product controller for single product
+app.controller('ProductCtrl', function ($scope, singleProduct) {
+    $scope.product = singleProduct;
+})
+
+// //products controller for all products
+app.controller('ProductsCtrl', function ($scope, allProducts) {
+    $scope.products = allProducts;
+});
