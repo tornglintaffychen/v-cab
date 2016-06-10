d1eae94b027a9dca719a2f10452f1ddb3da0fd0f
Svinci131 <svinci131@gmail.com>
2016-06-10 15:04:15 -0400

    moved to resolve

--- browser/js/checkout/checkout.js
+++ browser/js/checkout/checkout.js
@@ -6,9 +6,11 @@ app.config(function ($stateProvider) {
 		templateUrl: "js/checkout/checkout.html",
 		controller: "checkoutCtrl",
 		resolve: {
-			user: function (AuthService) {
+			user: function (AuthService, AddressFactory) {
+
 				return AuthService.getLoggedInUser()
 				.then(function(user){
+					user.fullName = AddressFactory.getFullName(user.firstName, user.lastName)
 					return (user);
 				});
 			}
@@ -19,12 +21,6 @@ app.config(function ($stateProvider) {
 app.controller("checkoutCtrl", function ($scope, AddressFactory, user, AuthService ) {
 	$scope.user = user;
 	$scope.address = user.address;
-
-	console.log("here", AddressFactory.getFullName($scope.user.firstName, $scope.user.lastName));
 	console.log(user);
-	// AuthService.getLoggedInUser()
-	// .then(function(user){
-	// 	console.log(user);
-	// });
 
 });
