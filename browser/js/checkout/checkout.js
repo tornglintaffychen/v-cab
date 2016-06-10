9349c74ebe10f0694e3b3d824f58a48a5c2cf44c
Svinci131 <svinci131@gmail.com>
2016-06-10 15:02:25 -0400

    wrote capatilize

--- browser/js/checkout/checkout.js
+++ browser/js/checkout/checkout.js
@@ -16,9 +16,11 @@ app.config(function ($stateProvider) {
 	});
 })
 
-app.controller("checkoutCtrl", function ($scope, user, AuthService) {
+app.controller("checkoutCtrl", function ($scope, AddressFactory, user, AuthService ) {
 	$scope.user = user;
 	$scope.address = user.address;
+
+	console.log("here", AddressFactory.getFullName($scope.user.firstName, $scope.user.lastName));
 	console.log(user);
 	// AuthService.getLoggedInUser()
 	// .then(function(user){
