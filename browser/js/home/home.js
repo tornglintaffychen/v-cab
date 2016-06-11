f61ac36d9670666526c9aabb4775f5c99f9f764d
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-06-11 15:00:38 -0400

    todo in home

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,6 +2,7 @@ app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
         templateUrl: 'js/home/home.html'
+				//TODO:
 				//considering using views: http://angular-ui.github.io/ui-router/site/#/api/ui.router.state.directive:ui-view
 				// views:{
 
