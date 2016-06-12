b27f38bf6ac6d834e9e572d7ddf870ed784c83c6
Torng-Lin Chen <tornglinchen@Torng-Lins-MacBook-Pro.local>
2016-06-12 17:13:11 -0400

    single prodcut page fully functioning

--- browser/js/catalog/product.js
+++ browser/js/catalog/product.js
@@ -4,12 +4,12 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('product', {
         url: '/products/:productId',
-        templateUrl: 'js/detail/templates/product.html',
+        templateUrl: 'js/catalog/product.html',
         controller: 'ProductCtrl',
         resolve: {
             //LA: changed from oneProduct to singleProduct
             singleProduct: function (ProductFactory, $stateParams) {
-                return ProductFactory.fetchById($stateParams.id);
+                return ProductFactory.fetchById($stateParams.productId)
             }
         }
     })
@@ -27,12 +27,14 @@ app.config(function ($stateProvider) {
             }
         }
     });
+
 });
 
 
 // //product controller for single product
-app.controller('ProductCtrl', function ($scope, singleProduct) {
+app.controller('ProductCtrl', function ($scope, singleProduct, CartFactory) {
     $scope.product = singleProduct;
+    $scope.addToCart = CartFactory.addToCart;
 })
 
 // //products controller for all products
