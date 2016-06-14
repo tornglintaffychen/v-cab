3ef6cfbac354de8d3d92c9b934f7205bdc30fadd
Torng-Lin Chen <tornglinchen@Torng-Lins-MacBook-Pro.local>
2016-06-14 17:25:54 -0400

    fixed next button

--- browser/js/common/factories/checkout.factory.js
+++ browser/js/common/factories/checkout.factory.js
@@ -1,40 +1,40 @@
-app.factory("CheckOutFactory", function($http, $state, CartFactory){
-	var mailOptions = {};
+app.factory("CheckOutFactory", function ($http, $state, CartFactory) {
+    var mailOptions = {};
 
-	function sendConfirmation (products) {
-		mailOptions.products = products;
-		return $http.post("/send/orderConfirmation",
-		mailOptions)
-		.then(function(res){
-			if (res.status === 200) {
-				CartFactory.getItems()
-				$state.go('home')
-			}
-			return res.data;
-		})
-	}
-	function setName (name) {
-		console.log("NAME", name);
-		mailOptions.name = name;
-	}
+    function sendConfirmation(products) {
+        mailOptions.products = products;
+        return $http.post("/send/orderConfirmation",
+                mailOptions)
+            .then(function (res) {
+                if (res.status === 200) {
+                    CartFactory.getItems()
+                    $state.go('home')
+                }
+                return res.data;
+            })
+    }
 
-	function setEmail (email) {
-		mailOptions.email = email;
-	}
+    function setName(name) {
+        mailOptions.name = name;
+    }
 
-	function setAddress (address) {
-		mailOptions.address = address;
-	}
+    function setEmail(email) {
+        mailOptions.email = email;
+    }
 
-	function getMailOptions () {
-		return mailOptions;
-	}
+    function setAddress(address) {
+        mailOptions.address = address;
+    }
 
-	return {
-		sendConfirmation: sendConfirmation,
-		setAddress: setAddress,
-		setEmail: setEmail,
-		setName: setName,
-		getMailOptions: getMailOptions
-	};
+    function getMailOptions() {
+        return mailOptions;
+    }
+
+    return {
+        sendConfirmation: sendConfirmation,
+        setAddress: setAddress,
+        setEmail: setEmail,
+        setName: setName,
+        getMailOptions: getMailOptions
+    };
 });
