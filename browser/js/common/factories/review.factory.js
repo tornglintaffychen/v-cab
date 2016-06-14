8b8ebfbaddfef38af11c3f91f7ea21d60b6c28d3
Katherine Buechner <kbuechner@gmail.com>
2016-06-14 10:51:54 -0400

    Revert "Merge branch 'css' of https://github.com/maxww/v-cab into css"
    This reverts commit c6f0731f38b2fedde2c23907eb84e7e608221662.

--- browser/js/common/factories/review.factory.js
+++ browser/js/common/factories/review.factory.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 app.factory('ReviewFactory', function ($http) {
-=======
-app.factory('ReviewFactory', function ($http, ProductFactory, UserFactory) {
-    //sv (str, str) => string
->>>>>>> 0651bf64de0a22b0cd89afc1d1018cfd0b61669b
     var ReviewFactory = {};
 
     ReviewFactory.addReview = function(review){
@@ -38,4 +33,4 @@ app.factory('ReviewFactory', function ($http, ProductFactory, UserFactory) {
 
     return ReviewFactory;
 
-})
+}
