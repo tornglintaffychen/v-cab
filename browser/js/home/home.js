2df9370194634a4f997d9dbb7d303a578a2fc364
Torng-Lin Chen <tornglinchen@Torng-Lins-MacBook-Pro.local>
2016-06-14 14:12:29 -0400

    styled home page and products page

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,11 +1,17 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
-				//TODO:
-				//considering using views: http://angular-ui.github.io/ui-router/site/#/api/ui.router.state.directive:ui-view
-				// views:{
-
-				// }
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl',
+        resolve:{
+            allProducts: function (ProductFactory) {
+                return ProductFactory.fetchAll();
+            }
+        }
     });
 });
+
+app.controller('HomeCtrl', function($scope, allProducts, CartFactory){
+    $scope.products = allProducts;
+    $scope.addToCart = CartFactory.addToCart;
+})
