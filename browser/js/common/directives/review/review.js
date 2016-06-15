76fe3d566f61e5c3425cc8a7c92a8f7aafa36f18
Katherine Buechner <kbuechner@gmail.com>
2016-06-15 09:50:07 -0400

    committing some stuff as of 9:50am

--- browser/js/common/directives/review/review.js
+++ browser/js/common/directives/review/review.js
@@ -1,11 +1,24 @@
-app.controller("ReviewController", function($scope, UserFactory) {
-  $scope.getUser = function(id){
-  	return UserFactory.fetchById()
-  	.then(function(foundUser){
-  		return foundUser.email;
-  	})
-  	.catch(function(error){
-  		console.error("something went wrong with fetching the user email! ", error)
-  	})
-  }
+
+app.controller("ReviewController", function($scope, $http, ReviewFactory){
+
+  $scope.newReview = function(uId, pId) {
+  	$scope.review.userId = uId;
+  	$scope.review.productId = pId;
+  return ReviewFactory.addReview($scope.review)
+  .then(function(review){
+    $scope.message = "Thank you for your review!";
+  })
+  .catch(function(error){
+    $scope.message = "There was a problem posting your review.";
+  })
+  };
+
+// // who cares about this 
+
+// id: number
+// rating: number
+// text: text
+$scope.logModel = function(){
+  console.log($scope.review)
+}
 })
--- browser/js/common/factories/review.factory.js
+++ browser/js/common/factories/review.factory.js
@@ -1,45 +1,35 @@
-app.factory('ReviewFactory', function ($http, ProductFactory, UserFactory) {
-    //sv (str, str) => string
+app.factory('ReviewFactory', function ($http) {
     var ReviewFactory = {};
 
-    // leaving this in to remind me of my deep shame at this wretched code - kb
-    // function parseDate(dateString){
-    //     var slicedDate = dateString.slice(0,9);
-    //     var year = dateString.slice(0, 4);
-    //     var month = dateString.slice(6, 8);
-    //     var day = dateString.slice(10, 12);
-    //     return month + "/" + day + "/" + year;
-    // }
-
-    //kb-priority: 
     ReviewFactory.addReview = function(review){
-        return $http.post('/api/reviews')
+        return $http.post('/api/reviews', review)
         .then(function(res){
-            console.log('here is the new review res: ', res)
+            console.log('here is the new review res: ' + res)
         })
         .catch(function(error){
-            console.error("something went wrong with posting your review! ", error)
+            console.error("something went wrong with posting your review! "  + error)
         })
     }
 
     ReviewFactory.editReview = function(id){
-        return $http.put('/api/reviews/'+ id )
+        return $http.put('/api/reviews/'+ id, review)
         .then(function(res){
-            console.log('here is the update res: ', res)
+            console.log('here is the update res: ' + res)
         })
         .catch(function(error){
-            console.error("something went wrong with editing your review! ", error)
+            console.error("something went wrong with editing your review! " + error)
         })
     }
+
     ReviewFactory.removeReview = function(id){
         return $http.delete('/api/reviews/' + id)
         .then(function(res){
-            console.log('here is the delete res: ', res)
+            console.log('here is the delete res: ' + res)
         })
         .catch(function(error){
-            console.error("somethingn went wrong with deleting your review! ", error)
+            console.error("something went wrong with deleting your review! " + error)
         })
     }
     return ReviewFactory;
-
 })
+
